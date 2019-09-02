8896d2c5a5e46a5b61b1030477d8cdfdf341125b
Avi <Avi@vetharest.wireless-psk.unc.edu>
2019-09-01 20:11:09 -0400

    Novice operational, adept can read store costs

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,4 +1,4 @@
-package a1;
+ package a1;
 
 import java.util.Scanner;
 
@@ -8,7 +8,35 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
+		int storeStockCount = scan.nextInt();
+		String[] storeStock = new String[storeStockCount];
+		double[] storePrices = new double[storeStockCount];
+		for (int i = 0; i < storeStockCount ; i++) {
+			storeStock[i] = scan.next();
+			storePrices[i] = scan.nextDouble();
+		}
+		
+		int numberCustomers = scan.nextInt();
+		
+		
 		// Your code follows here.
 		
+		scan.close();
+	}
+	
+	
+	
+	
+	// stolen from lec04 example 2 so I could test some stuff out, sorry if I forgot to delete this
+	public static void print_array(String label, double[] array_to_print) {
+		System.out.print(label + ": ");
+		for (int i=0; i<array_to_print.length; i++) {
+			System.out.print(array_to_print[i]);
+
+			if (i != array_to_print.length-1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println();
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -1,5 +1,7 @@
 package a1;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class A1Novice {
@@ -7,8 +9,41 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		int numberCustomers = scan.nextInt();
+		String[] finalOutput = new String[numberCustomers];
 
-		// Your code follows here.
+		
+		for (int i = 0 ; i < numberCustomers ; i++) {
+			// for each customer, check their name and initialize
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numberItems = scan.nextInt();
+			double personalCost = 0;
+			
+			
+
+			for (int j = 0 ; j < numberItems ; j++) {
+				// for each item, calculate cost
+				int itemCount = scan.nextInt();
+				scan.next(); // name of item is irrelevant
+				double itemPrice = scan.nextDouble();
+				personalCost += itemCount * itemPrice ;
+			}
+			
+			
+			finalOutput[i] = Character.toString(firstName.charAt(0)) + ". " + lastName + ": " + 
+					String.format("%.2f", personalCost);
+
+			
+		}
+
+		scan.close();
+		
+		for (int k = 0 ; k < finalOutput.length ; k++) {
+			System.out.println(finalOutput[k]);
+		}
+		
+		// """""Your code follows here.""""" xd
 		
 	}
 }
